41ac537339e72676a4d776861563f80e361146b2
Alexandrescu Andrei-Robert <ar_alexandrescu@yahoo.com>
2021-10-30 19:54:31 +0300

    Changed title

--- src/Components/Images/styles.js
+++ src/Components/Images/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core";
 export const useStyles = makeStyles({
     box: {
         margin: '4px auto',
-        display: 'flex'
+        display: 'flex',
+
     }
 });
