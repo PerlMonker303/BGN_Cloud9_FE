2e33089157abd12f0d0734a7b27afa0259080fa8
Alexandrescu Andrei-Robert <ar_alexandrescu@yahoo.com>
2021-10-30 14:16:50 +0300

    Finished search bar

---
+++ src/Components/SearchBar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { useStyles } from "./styles";
+import { TextField, Input } from "@material-ui/core";
+
+const SearchBar = ({ keyword, setKeyword }) => {
+    const classes = useStyles();
+
+    const handleTextChanged = (e) => {
+        setKeyword(e.target.value);
+    }
+
+    return (
+        <Input placeholder="Search for a keyword" onChange={handleTextChanged} />
+    );
+};
+
+export default SearchBar;
